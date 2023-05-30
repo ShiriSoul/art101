9cf6aa61d70cef4c1a2d9eef8abdce4dacde8f54
Tony P <topau@ucsc.edu>
2023-05-29 22:32:56 -0700

    lab 14 updated again

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,5 +1,5 @@
 $("#my-button").click(function() {
-    let x = 15 * 4;
+    let x = 23 * 6;
     debugger;   //debugger will prevent the next line from running if console or debugger is open
-    $("#output").html("15 * 5 = " + x);
+    $("#output").html("23 * 6 = " + x);
 })
