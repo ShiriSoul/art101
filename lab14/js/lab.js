1b6013d7cfe1f1d72360c854aa9f3a99da721785
Tony P <topau@ucsc.edu>
2023-05-29 23:11:54 -0700

    lab 14 js fixed

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,3 +1,7 @@
+// lab.js - This js file is to experiment breakpoints with debugger
+// Author: Tony Pau
+// Date: May 29, 2023
+
 $("#my-button").click(function() {
     let x = 23 * 6;
     debugger;   //debugger will prevent the next line from running if console or debugger is open
