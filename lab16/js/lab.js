dd69240b09aa1414a0d972c67eed8f3e596fdc2b
Tony P <topau@ucsc.edu>
2023-06-06 00:41:06 -0700

    updated16
    16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,54 +2,51 @@
 // Author: Tony Pau
 // Date: June 5, 2023
 
-$(document).ready(function() {
-    // Prompt user for input
-    var userDate = prompt("Enter a date (YYYY-MM-DD) to see the NASA picture of the day:");
-  
-    // Validate user input using moment
-    var isValidDate = moment(userDate, "YYYY-MM-DD", true).isValid();
-  
-    if (!isValidDate) {
-      console.log("Invalid date format. Using the current date instead.");
-      userDate = moment().format("YYYY-MM-DD");
-      $("#output").append("<h2>Since an invalid date was given, here is today's photo: </h2>");
+var startURL = "https://xkcd.com/";
+var endURL = "info.0.json";
+
+function getAndPutData(num) {
+    if (typeof num === 'undefined') {
+        numStr = "";
     }
-  
-    // Using the core $.ajax() method
-    $.ajax({
-        // The URL for the request (from the API docs)
-        url: "https://api.nasa.gov/planetary/apod?api_key=KheLziIIVaw5qZSrvPYLbIQXuGi9KO0MrYKa3o4F",
-        // The data to send (will be converted to a query string)
-        data: { 
-            date: userDate // Use the user's input as the date parameter
-        },
-        // Whether this is a POST or GET request
+    else {
+        numStr = num.toString() + "/";
+    }
+    var trueURL = startURL + numStr + endURL;
+    console.log("Here is the URL: ", trueURL);
+    $.ajax ({
+        url: trueURL,
+        data: {},
         type: "GET",
-        // The type of data we expect back
         dataType: "json",
-        // What do we do when the API call is successful
-        // All the action goes in here
         success: function(data) {
-            // do stuff
-            nasaObj = data;
-            nasaImgUrl = data.hdurl;
-            nasaDate = data.date;
-            nasaExplain = data.explanation;
-  
-            console.log(data.date);
-            console.log(data.explanation);
-            if (isValidDate){
-                $("#output").append("<h2>Your Picture of The Day</h2>"); // so that this is appended only if the date given was valid
-            }
-            $("#output").append("<h2>" + nasaDate + "</h2>");
-            $("#output").append("<p>" + nasaExplain + "</p>");
-            $("#output").append("<img src='" + nasaImgUrl + "'>");
+            console.log(data);
+            var comicObj = data;
+            var html = 
+            `
+            <div id="comic">
+                <h2>${comicObj.title}</h2>
+                <img src="${comicObj.img}" title="${comicObj.alt}"><br>
+                <button id="prev-button">Previous</button>
+                <button id="next-button">Next</button>
+            </div>
+            `;
+            $("#output").html(html);
+
+            // button prev
+            $("#prev-button").click(function() {
+                getAndPutData(comicObj.num - 1);
+            })
+
+            // button next
+            $("#next-button").click(function() {
+                getAndPutData(comicObj.num + 1);
+            })
         },
-        // What we do if the API call fails
-        error: function(jqXHR, textStatus, errorThrown) { 
-            // do stuff
+        error: function(jqXHR, textStatus, errorThrown) {
             console.log("Error:", textStatus, errorThrown);
         }
-    });
-  
-});
+    })
+}
+
+getAndPutData();
